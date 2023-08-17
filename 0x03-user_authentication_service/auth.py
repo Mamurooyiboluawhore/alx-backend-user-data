447ae48129c4bb91592c13141e52ac5b1be971e8
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-17 04:33:55 +0000

    basic flask app

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 ''' Hash password '''
 import bcrypt
-
+from db import DB
+from user import User
 
 def _hash_password(password: str) -> bytes:
     ''' a function that hashes password'''
@@ -10,3 +11,19 @@ def _hash_password(password: str) -> bytes:
 
     hashed_password = bcrypt.hashpw(passWord.encode('utf-8'), salt)
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ a function that registers users
+        """
+        user = self._db.find_user_by(email=email)
+        hashed_password = _hash_password(password)
+        if user:
+            raise ValueError (f'User {email} already exists')
+            return _db.add_user(email, hashed_password) 
