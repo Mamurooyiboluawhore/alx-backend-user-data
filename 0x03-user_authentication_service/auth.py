e3d3878dd9623d538fdcc9e88d68c3798ed73b49
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-17 01:56:26 +0000

    hashed a password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' Hash password '''
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    ''' a function that hashes password'''
+    salt = bcrypt.gensalt()
+    passWord = password
+
+    hashed_password = bcrypt.hashpw(passWord.encode('utf-8'), salt)
+    return hashed_password, salt
