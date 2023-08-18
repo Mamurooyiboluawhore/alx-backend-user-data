94612495b8e5e5adc8c6bd5c1d217ee4194e4fe3
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-08-18 23:55:04 +0000

    made it pycode complaint

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 
+
 def _hash_password(password: str) -> bytes:
     ''' a function that hashes password'''
     salt = bcrypt.gensalt()
@@ -11,6 +12,7 @@ def _hash_password(password: str) -> bytes:
 
     hashed_password = bcrypt.hashpw(passWord.encode('utf-8'), salt)
     return hashed_password
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -25,5 +27,9 @@ class Auth:
         user = self._db.find_user_by(email=email)
         hashed_password = _hash_password(password)
         if user:
-            raise ValueError (f'User {email} already exists')
-        return self._db.add_user(email, hashed_password) 
+            raise ValueError(f'User {email} already exists')
+        return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        #email = _db
+        pass
